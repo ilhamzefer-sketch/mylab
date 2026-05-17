0cdd20cd168b4791ba84ae4490ecd0dccb67b302
Ilham Safarov <seferov.ilham@internet.ru>
2026-05-17 09:35:19 +0400

    first commit

---
+++ src/test/java/az/mylab/MylabApplicationTests.java
@@ -0,0 +1,13 @@
+package az.mylab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MylabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
